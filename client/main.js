ec152bb53bfead32fb11db977d557da2985f3f53
BenjaminGGardner <benjamingardner73@gmail.com>
2022-04-12 15:39:56 +0100

    Comments not working

--- client/main.js
+++ client/main.js
@@ -2,6 +2,5 @@ import React from 'react'
 import { hydrate } from 'react-dom'
 import App from './App'
 import ReactDOM from "react-dom";
-import css from "./index.css";
 
 hydrate(<App/>, document.getElementById('root'))
